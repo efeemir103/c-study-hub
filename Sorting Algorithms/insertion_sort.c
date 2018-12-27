8667f6b296369a6a5eddb93ed8ff9050d21b14e7
Efe Emir Pekmez <efeemir103@gmail.com>
2018-12-27 20:56:37 +0300

    Create insertion_sort.c

---
+++ Sorting Algorithms/insertion_sort.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void createArr(int arr[],int size);
+void printArr(int arr[],int size);
+void sortArr(int arr[],int size);
+
+int main(){
+  int arr[10],size=10;
+  createArr(arr,size);
+  sortArr(arr,size);
+  printArr(arr,size);
+  return 0;
+}
+
+void createArr(int arr[],int size){
+  int i;
+  for(i=0;i<size;i++){
+    scanf("%d",&arr[i]);
+  }
+}
+
+void printArr(int arr[],int size){
+  int i;
+  for(i=0;i<size;i++){
+    printf("Arr[%d] = [%d]\n",i,arr[i]);
+  }
+}
+
+void sortArr(int arr[],int size){
+
+}
