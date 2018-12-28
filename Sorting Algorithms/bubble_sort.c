d88ee00333f50faf7cd89e996eeda26976fe6962
Efe Emir Pekmez <efeemir103@gmail.com>
2018-12-28 04:37:12 +0300

    Update and rename bubble_sort_array.c to bubble_sort.c

--- Sorting Algorithms/bubble_sort.c
+++ Sorting Algorithms/bubble_sort.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-void createArr(int *arr,int size);
-void printArr(int *arr,int size);
+void createArr(int arr[],int size);
+void printArr(int arr[],int size);
 void sortArr(int array[],int size);
 
 int main(){
@@ -12,14 +12,14 @@ int main(){
   return 0;
 }
 
-void createArr(int *arr,int size){
+void createArr(int arr[],int size){
   int i;
   for(i=0;i<size;i++){
     scanf("%d",&arr[i]);
   }
 }
 
-void printArr(int *arr,int size){
+void printArr(int arr[],int size){
   int i;
   for(i=0;i<size;i++){
     printf("Arr[%d] = [%d]\n",i,arr[i]);
@@ -29,9 +29,9 @@ void printArr(int *arr,int size){
 void sortArr(int array[],int size){
   int i,j,temp;
   for(i=0;i<size;i++){
-    for(j=0;j<size-1;j++){
-      if(array[j]>array[j+1]){
-        temp=array[j];
+    for(j=0;j<size-1;j++){//checking if n varibles for minimum requires n-1 check to be sure.
+      if(array[j]>array[j+1])
+        temp=array[j];//we need third varible to remember the value of the variable that will be changed.
         array[j]=array[j+1];
         array[j+1]=temp;
       }
