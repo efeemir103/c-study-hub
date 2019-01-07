6facc5ce4b6f9e25307f06c5ccaa90ab70647aa5
Efe Emir Pekmez <efeemir103@gmail.com>
2019-01-08 01:53:51 +0300

    Update strlen.c

--- String Functions From Stratch/strlen.c
+++ String Functions From Stratch/strlen.c
@@ -9,7 +9,7 @@ int main(){
 
 int strlen(char str[]){
   int size=0;
-  while(str[size]){
+  while(str[size]){//if str[size] == NULL then str[size] == '\0' and '0' == 0
     size++;
   }
   return size;
