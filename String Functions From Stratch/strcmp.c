7950330474269f7664ccb43aea2f657db9ab1611
Efe Emir Pekmez <efeemir103@gmail.com>
2019-01-08 02:09:42 +0300

    Create strcmp.c

---
+++ String Functions From Stratch/strcmp.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int strcmp(char dest[],char src[]);
+
+int main(){
+  printf("Compare result is: %d",strcmp("abc","abd"));
+  return 0;
+}
+ 
+int strcmp(char dest[],char src[]){
+  int i,lengthOfDest=0,lengthOfSrc=0,minLength;
+  while(dest[lengthOfDest]){
+    lengthOfDest++;
+  }
+  while(src[lengthOfSrc]){
+    lengthOfSrc++;
+  }
+  minLength=lengthOfDest<lengthOfSrc?lengthOfDest:lengthOfSrc;
+  for(i=0;i<minLength;i++){
+    if(dest[i]<src[i])
+      return -1;
+    if(dest[i]>src[i])
+      return +1;
+  }
+  return 0;
+}
